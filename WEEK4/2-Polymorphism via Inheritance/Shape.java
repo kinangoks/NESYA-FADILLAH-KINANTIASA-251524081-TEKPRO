118a79de5a392d6dfd60cd6405473b3f1b8251d8
kinangoks <nesya.fadillah.tif425@polban.ac.id>
2026-03-05 22:42:29 +0700

    Add files via upload

---
+++ WEEK4/2-Polymorphism via Inheritance/Shape.java
@@ -0,0 +1,15 @@
+public abstract class Shape {
+    private String shapeName;
+
+    public Shape(String name)
+    {
+        this.shapeName = name;
+    }
+
+    public abstract double area();
+
+    public String toString()
+    {
+        return shapeName;
+    }
+}
